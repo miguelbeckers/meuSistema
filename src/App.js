036cd202f6a8eaf14993a6876e96c44071bff3d6
miguelbeckers <52552717+miguelbeckers@users.noreply.github.com>
2021-02-20 00:05:27 -0300

    outro commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edite <code>src/App.js</code> e salve para recomeçar.
+          Edite <code>src/App.js</code> e salve para recarregaree.
         </p>
         <a
           className="App-link"
